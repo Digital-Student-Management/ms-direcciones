13cf1c8f7efe8ab56f1ff489543369842eaf4571
Darío Meza <dari.meza@duocuc.cl>
2026-05-16 15:24:56 -0400

    chore: inicializar estructura base del microservicio

---
+++ src/test/java/com/ms_sistemaEscolar/ms_direcciones/MsDireccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_sistemaEscolar.ms_direcciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDireccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
